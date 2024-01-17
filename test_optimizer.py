82787bfcef577d4dc76dd9c9e52e2d14a7629dc4
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2024-01-16 20:01:25 -0500

    Created failing test

--- test_optimizer.py
+++ test_optimizer.py
@@ -7,12 +7,24 @@ class TestOptimizer(unittest.TestCase):
         optimizer = StructureOptimizer(materials)
         structure = optimizer.largest_volume_structure()
         self.assertEqual(1, structure.volume())
-    
-    def test_make_rectangle(self):
+
+    def test_make_2_volume_rectangle(self):
         materials = [Material(1), Material(1), Material(2)]
         optimizer = StructureOptimizer(materials)
         structure = optimizer.largest_volume_structure()
         self.assertEqual(2, structure.volume())
+
+    def test_make_4_volume_rectangle(self):
+        materials = [Material(1), Material(2), Material(2)]
+        optimizer = StructureOptimizer(materials)
+        structure = optimizer.largest_volume_structure()
+        self.assertEqual(4, structure.volume())
+    
+    def test_make_64_volume_cube(self):
+        materials = [Material(2), Material(2), Material(2), Material(2), Material(2), Material(2)]
+        optimizer = StructureOptimizer(materials)
+        structure = optimizer.largest_volume_structure()
+        self.assertEqual(64, structure.volume())
 
 def main():
     unittest.main()
